bde12fc777669d45a37d3df3cafd6e4729993fca
cti1650 <cti1650@gmail.com>
2021-02-12 07:54:22 +0900

    test

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+router.get("/api", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.send({ name: "太郎" });
+  console.log("get");
+});
+
+router.post("/api", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.send({ name: "太郎" });
+  console.log("get");
+});
+
+router.post("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Content-Type", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.send({ name: "太郎" });
+  console.log("プリフライト");
+});
+router.options("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Content-Type", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.send();
+});
 module.exports = router;
